1464fbeee237a58c03a4560d583349283115c212
Daimler Clavel <dmclavel@up.edu.ph>
2019-09-18 12:26:58 +0800

    Added back ecommerce for testing

--- js/script.js
+++ js/script.js
@@ -173,20 +173,20 @@ $(document).ready(function() {  // when the page is loaded, call superslides fun
     });
 
     $('#github-click').on('click', function () {
-        // window.dataLayer.push({
-        //     'event': 'github-click',
-        //     'dateClicked': new Date().toString(),
-        //     'transactionId': '1001',
-        //     'transactionTotal': 3000,
-        //     'transactionProducts': [
-        //         {
-        //             'sku': 'GH1',
-        //             'name': 'Github Link Trial',
-        //             'price': 3000,
-        //             'quantity': 1
-        //         }
-        //     ]
-        // });
+        window.dataLayer.push({
+            'event': 'github-click',
+            'dateClicked': new Date().toString(),
+            'transactionId': '1001',
+            'transactionTotal': 3000,
+            'transactionProducts': [
+                {
+                    'sku': 'GH1',
+                    'name': 'Github Link Trial',
+                    'price': 3000,
+                    'quantity': 1
+                }
+            ]
+        });
     });
 });
 
